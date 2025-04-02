59369485f5bcaee3eb4f21ccbaaa1a2e7b156c16
Daniel Zhu <daniel_zhu1@brown.edu>
2025-04-02 18:45:59 -0400

    framework for map reduce getText

--- distribution/all/search-engine/search.js
+++ distribution/all/search-engine/search.js
@@ -3,11 +3,36 @@ function search(config) {
   const context = {};
   context.gid = config.gid || 'all';
   context.hash = config.hash || global.distribution.util.id.naiveHash;
+  function setup(configuration, callback) {
+    // Assume these are the endpoints for the book txts.
+    const dataset = configuration['data']
+    const gid = config["gid"];
+    function getText() {
+      const mapper = (key, value) => {
+        
+      };
+    
+      const reducer = (key, values) => {
+       
+      };  
+
+      distribution[gid].mr.exec({keys: Object.keys(dataset), map: mapper, reduce: reducer}, (e, v) => {
+        try {
+          expect(v).toEqual(expect.arrayContaining(expected));
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+      
+    }
+
+    callback(null, configuration);
+
+  }
 
   return {
-    setup: (configuration, callback) => {
-        callback(null, configuration);
-    },
+    setup,
 
     query: (configuration, callback) => {
         callback(null, configuration);
